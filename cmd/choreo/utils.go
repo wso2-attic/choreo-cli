b38a51e7c5d7d82b89f3a73a6239e12ec012fe47
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2019-09-04 10:32:22 +0530

    Refactor util method used to exist with error message

--- cmd/choreo/utils.go
+++ cmd/choreo/utils.go
@@ -21,7 +21,12 @@ func getAbsoluteCommandName(command string) string {
 	return commandRoot + " " + command
 }
 
-func exitWithErrorMessage(message string, err error) {
+func exitWithError(message string, err error) {
 	fmt.Printf("\n\n%s: %v\n\n", message, err)
+	os.Exit(1)
+}
+
+func exitWithErrorMessage(message string) {
+	fmt.Println(message)
 	os.Exit(1)
 }
