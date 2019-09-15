3e26251e34f8ba4d7d4bc6ca60e46925f61f7240
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2019-09-15 18:17:34 +0530

    Update license header

--- internal/pkg/cmd/login/login.go
+++ internal/pkg/cmd/login/login.go
@@ -3,11 +3,8 @@
  *
  * This software is the property of WSO2 Inc. and its suppliers, if any.
  * Dissemination of any information or reproduction of any material contained
- * herein is strictly forbidden, unless permitted by WSO2 in accordance with
- * the WSO2 Commercial License available at http://wso2.com/licenses. For specific
- * language governing the permissions and limitations under this license,
- * please see the license as well as any agreement you’ve entered into with
- * WSO2 governing the purchase of this software and any associated services.
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
 package login
