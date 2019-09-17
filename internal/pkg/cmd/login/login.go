59f57f36be7763bac01c04fe38bb2e2015bc8944
Asanka Abeyweera <asankaab@wso2.com>
2019-09-17 13:57:15 +0530

    Refactor code

--- internal/pkg/cmd/login/login.go
+++ internal/pkg/cmd/login/login.go
@@ -33,17 +33,16 @@ func NewLoginCommand(cliConfig config.Config) *cobra.Command {
 
 func createLoginFunction(cliConfig config.Config) func(cmd *cobra.Command, args []string) {
 	getEnvConfig := config.GetEnvironmentConfigReader(cliConfig, envConfigs)
-	setUserConfig := config.GetUserConfigWriter(cliConfig,userConfigs)
+	setUserConfig := config.GetUserConfigWriter(cliConfig, userConfigs)
 
 	return func(cmd *cobra.Command, args []string) {
-		authCode, oauth2Conf := getAuthCode(getEnvConfig)
+		codeServicePort := common.GetFirstOpenPort(callBackDefaultPort)
+		oauth2Conf := createOauth2Conf(callbackUrlContext, codeServicePort, getEnvConfig)
+		authCodeChannel, server := startAuthCodeReceivingService(codeServicePort, oauth2Conf, setUserConfig)
+		openBrowserForAuthentication(oauth2Conf)
+		<-authCodeChannel
+		stopAuthCodeServer(server)
 
-		token, err := getAccessToken(authCode, oauth2Conf)
-		if err != nil {
-			common.ExitWithError("Could not get an access token", err)
-		}
-
-		setUserConfig(accessToken, token)
 		common.PrintInfo("Successfully logged in to " + common.ProductName + ".")
 	}
 }
@@ -58,16 +57,6 @@ func getAccessToken(authCode string, conf *oauth2.Config) (string, error) {
 	}
 }
 
-func getAuthCode(getEnvConfig config.GetConfig) (string, *oauth2.Config) {
-	codeServicePort := common.GetFirstOpenPort(callBackDefaultPort)
-	authCodeChannel, server := startAuthCodeReceivingService(codeServicePort)
-	oauth2Conf := openBrowserForAuthentication(callbackUrlContext, codeServicePort, getEnvConfig)
-	authCode := <-authCodeChannel
-	stopAuthCodeServer(server)
-
-	return authCode, oauth2Conf
-}
-
 func stopAuthCodeServer(server *http.Server) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
@@ -76,73 +65,84 @@ func stopAuthCodeServer(server *http.Server) {
 	}
 }
 
-func startAuthCodeReceivingService(port int) (<-chan string, *http.Server) {
-	authCodeChannel := make(chan string)
+func startAuthCodeReceivingService(port int, oauth2Conf *oauth2.Config, setUserConfig config.SetConfig) (<-chan bool, *http.Server) {
+	oauthDone := make(chan bool)
 
 	mux := http.NewServeMux()
 	server := &http.Server{Addr: common.GetLocalBindAddress(port), Handler: mux}
 	mux.HandleFunc(callbackUrlContext, func(writer http.ResponseWriter, request *http.Request) {
 		if err := request.ParseForm(); err != nil {
-			message := "Login to Choreo failed due to internal error. Please trying again using the CLI."
+			message := "Login to Choreo failed due to an internal error. Please trying again."
 			sendBrowserResponse(writer, http.StatusBadRequest, message)
-
-			common.PrintError("Error parsing received query parameters", err)
+			common.PrintError("Login to Choreo failed due to an error parsing the received query parameters", err)
+			oauthDone <- false
+			return
 		}
 
 		code := request.Form.Get("code")
 
 		if code == "" {
-			message := "Login to Choreo failed. Please trying again using the CLI."
+			message := "Login to Choreo failed due to an internal error. Please trying again."
 			sendBrowserResponse(writer, http.StatusBadRequest, message)
-			common.PrintErrorMessage("Blank auth code received from IDP")
+			common.PrintErrorMessage("Login to Choreo failed due to receiving a blank auth code from IDP")
+			oauthDone <- false
+			return
 		} else {
-			sendBrowserResponse(writer, http.StatusOK, "Login to Choreo is successful. Please return to the CLI.")
+			if err := exchangeAuthCodeForToken(code, oauth2Conf, writer, setUserConfig); err != nil {
+				message := "Login to Choreo failed due to internal error. Please trying again using the CLI."
+				sendBrowserResponse(writer, http.StatusBadRequest, message)
+				common.PrintError("Could not exchange auth code for an access token", err)
+			}
 		}
 
-		authCodeChannel <- code
+		oauthDone <- true
 	})
 
 	go listenForAuthCode(server)
 
-	return authCodeChannel, server
+	return oauthDone, server
+}
+
+func exchangeAuthCodeForToken(code string, oauth2Conf *oauth2.Config, writer http.ResponseWriter, setUserConfig config.SetConfig) error {
+	token, err := getAccessToken(code, oauth2Conf)
+	if err != nil {
+		return err
+	}
+	setUserConfig(accessToken, token)
+	sendBrowserResponse(writer, http.StatusOK, "Login to Choreo is successful. Please return to the CLI.")
+	return nil
 }
 
 func sendBrowserResponse(writer http.ResponseWriter, status int, message string) {
 	writer.WriteHeader(status)
 	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	content := ` <!DOCTYPE html>
-			<html>
-			<head>
-			<meta charset="UTF-8">
-			<title>CLI Login</title>
-			</head>
-			<body>
-			<h2>%s</h2>
-			</body>
-			</html> `
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>CLI Login</title>
+  </head>
+  <body>
+    <h2>%s</h2>
+  </body>
+</html> `
 
 	if _, err := fmt.Fprintf(writer, content, message); err != nil {
 		common.PrintError("Error while sending response to auth code redirect", err)
 	}
 }
 
-func openBrowserForAuthentication(context string, port int, getEnvConfig config.GetConfig) *oauth2.Config {
+func openBrowserForAuthentication(conf *oauth2.Config) {
+	hubAuthUrl := conf.AuthCodeURL("state")
+	if err := common.OpenBrowser(hubAuthUrl); err != nil {
+		common.ExitWithError("Couldn't open browser for "+common.ProductName+"  login", err)
+	}
+}
+
+func createOauth2Conf(context string, port int, getEnvConfig config.GetConfig) *oauth2.Config {
 	callBackUrlTemplate := "http://localhost:%d" + context
 	redirectUrl := fmt.Sprintf(callBackUrlTemplate, port)
-	conf := createOauth2Conf(redirectUrl, getEnvConfig)
-	hubAuthUrl := getHubUrl(conf)
-	if err := common.OpenBrowser(hubAuthUrl); err != nil {
-		common.ExitWithError("Couldn't open browser for " + common.ProductName + "  login", err)
-	}
 
-	return conf
-}
-
-func getHubUrl(conf*oauth2.Config) string {
-	return conf.AuthCodeURL("state")
-}
-
-func createOauth2Conf(redirectUrl string, getEnvConfig config.GetConfig) *oauth2.Config {
 	conf := &oauth2.Config{
 		ClientID:    getEnvConfig(clientId),
 		RedirectURL: redirectUrl,
