00adb175b05fc892b165cdc36a00cbd883378a6e
TharinduWijewardane <tharinduw@wso2.com>
2019-09-17 18:34:05 +0530

    Add client helper functions

---
+++ internal/pkg/client/config.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+package client
+
+import "github.com/wso2/choreo/components/cli/internal/pkg/config"
+
+const (
+	backendUrl = iota
+	skipVerify
+	accessToken
+)
+
+func CreateEnvironmentConfigReader(cliConfig config.Config) func(entry int) string {
+	return config.GetEnvironmentConfigReader(cliConfig, map[int]config.KeyEntry{
+		backendUrl: {
+			Key:          "choreo.backend.url",
+			DefaultValue: "https://api.choreo.dev:8081",
+		},
+		skipVerify: {
+			Key:          "security.certificate.skipVerify",
+			DefaultValue: "false",
+		},
+	})
+}
+
+func CreateUserConfigReader(cliConfig config.Config) func(entry int) string {
+	return config.GetUserConfigReader(cliConfig, map[int]config.KeyEntry{
+		accessToken: {
+			Key:          "login.oauth2.accessToken",
+			DefaultValue: "",
+		},
+	})
+}
---
+++ internal/pkg/client/helper.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+package client
+
+import (
+	"crypto/tls"
+	"github.com/wso2/choreo/components/cli/internal/pkg/config"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+// set InsecureSkipVerify option if required
+func NewClient(cliConfig config.Config) *http.Client {
+
+	getEnvConfig := CreateEnvironmentConfigReader(cliConfig)
+	skipVerify, _ := strconv.ParseBool(getEnvConfig(skipVerify))
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: skipVerify},
+	}
+	return &http.Client{Transport: tr}
+}
+
+// creates the http request for the given path with Authorization header set
+func NewRequest(cliConfig config.Config, method, path string, body io.Reader) (*http.Request, error) {
+
+	getEnvConfig := CreateEnvironmentConfigReader(cliConfig)
+	getUserConfig := CreateUserConfigReader(cliConfig)
+
+	completeUrl := getEnvConfig(backendUrl) + path
+	req, err := http.NewRequest(method, completeUrl, body)
+
+	if err == nil {
+		req.Header.Set("Authorization", "Bearer "+getUserConfig(accessToken))
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	return req, err
+}
