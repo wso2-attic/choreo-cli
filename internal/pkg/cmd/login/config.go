e2469e5e761e628c2ffefcd5816925cc76591769
Asanka Abeyweera <asankaab@wso2.com>
2019-09-17 12:07:38 +0530

    Change IDP client ID

--- internal/pkg/cmd/login/config.go
+++ internal/pkg/cmd/login/config.go
@@ -21,7 +21,7 @@ const (
 var envConfigs = map[int]config.KeyEntry{
 	clientId: {
 		Key:          "login.oauth2.clientId",
-		DefaultValue: "bLirYyUTmo9HJ1VlZ43vzahs6gca",
+		DefaultValue: "choreocliapplication",
 	},
 	authUrl: {
 		Key:          "login.oauth2.authUrl",
